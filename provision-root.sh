654d41f51383e01674a2acaa975c532d06364073
Decklin Foster <decklin@red-bean.com>
2014-05-08 17:44:54 -0400

    Use our fork of chruby/ruby-install (instead of hacking setup script)

--- provision-root.sh
+++ provision-root.sh
@@ -11,14 +11,11 @@ sudo apt-get -y upgrade
 # Other packages we need
 sudo apt-get install -q -y git vim nodejs sqlite3 libsqlite3-dev build-essential
 
-# Ruby
+# Ruby (our fork of chruby will not automatically build all rubies)
 cd /tmp
-curl -L https://github.com/postmodern/chruby/archive/v0.3.7.tar.gz | tar xzv
-cd chruby-0.3.7
-# FIXME: ugly hack to avoid spending 30min building JRuby/Rubinius and bulking up the image with their dependencies
-sed -i~ -e 's!^./setup.sh$!make install; ruby-install ruby!' ./scripts/setup.sh
-./scripts/setup.sh
-cd ..
+curl -L https://github.com/railsbridge-boston/chruby/archive/v0.3.8.rbb201405.3.tar.gz | tar xzv
+(cd chruby-0.3.8.rbb201405.3 && ./scripts/setup.sh)
+ruby-install ruby
 
 # Heroku Toolbelt is a .deb package, so install as root.
 curl -L https://toolbelt.heroku.com/install-ubuntu.sh | sh
