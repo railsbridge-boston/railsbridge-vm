5a707c19bef541b0debe62fdc794d2144c503843
Decklin Foster <decklin@red-bean.com>
2014-05-13 12:17:22 -0400

    Verbosify provision scripts, clean up, make chruby version a variable

--- provision-root.sh
+++ provision-root.sh
@@ -1,31 +1,36 @@
 # RailsBridge VM provision script
 
+# Set versions here
 RAILSBRIDGE_RUBY_VERSION='2.0'
+RAILSBRIDGE_CHRUBY_VERSION='0.3.8.rbb201405.4'
 
-# Ubuntu images now include things we don't want
+# Tell the shell to print commands before running them
+set -v
+
+# Remove configuration-management systems preinstalled in official Ubuntu images
 apt-get -y remove --purge chef chef-zero puppet puppet-common landscape-client landscape-common
+# And any dependencies
 apt-get -y autoremove
-
 # Get any security updates not in the base image
 sudo apt-get update
 sudo apt-get -y upgrade
 # Other packages we need
 sudo apt-get install -q -y git vim nodejs sqlite3 libsqlite3-dev
 
-# Ruby (our fork of chruby will not automatically build all rubies)
-cd /tmp
-curl -L https://github.com/railsbridge-boston/chruby/archive/v0.3.8.rbb201405.4.tar.gz | tar xzv
-(cd chruby-0.3.8.rbb201405.4 && ./scripts/setup.sh)
-ruby-install ruby "$RAILSBRIDGE_RUBY_VERSION" -- --disable-install-rdoc
-
-# Heroku Toolbelt is a .deb package, so install as root.
-curl -L https://toolbelt.heroku.com/install-ubuntu.sh | sh
-
 # Copy our files into place
 rsync -rtv /vagrant/etcfiles/ /etc
 rsync -rtv /vagrant/binfiles/ /usr/local/bin
 # Force MOTD generation (will only work on 14.04)
 run-parts --lsbsysinit /etc/update-motd.d > /run/motd.dynamic
+
+# Build/install Ruby (our fork of chruby will not automatically build all rubies)
+cd /tmp
+curl -L "https://github.com/railsbridge-boston/chruby/archive/v$RAILSBRIDGE_CHRUBY_VERSION.tar.gz" | tar xzv
+(cd "chruby-$RAILSBRIDGE_CHRUBY_VERSION" && ./scripts/setup.sh)
+ruby-install ruby "$RAILSBRIDGE_RUBY_VERSION" -- --disable-install-rdoc
+
+# Install Heroku Toolbelt (this adds an apt source, so must run as root)
+curl -L https://toolbelt.heroku.com/install-ubuntu.sh | sh
 
 # Clean up APT cache and zero out disk to reduce image size
 apt-get clean
--- provision-user.sh
+++ provision-user.sh
@@ -1,6 +1,14 @@
 # RailsBridge VM provision script (user part)
 
+# Set versions here
 RAILSBRIDGE_RAILS_VERSION='4.0.5'
+
+# Put the right `gem` in our PATH (do this before turning on -v)
+. /usr/local/share/chruby/chruby.sh
+chruby ruby-2.0
+
+# Tell the shell to print commands before running them
+set -v
 
 # Copy files that should be owned by the user account
 rsync -rtv /vagrant/dotfiles/ /home/vagrant
@@ -8,9 +16,6 @@ rsync -rtv /vagrant/dotfiles/ /home/vagrant
 # Our bash setup will cd to workspace on login.
 cd /home/vagrant
 ln -s /vagrant workspace
-
-# This is ugly, but we need to run the right gem
-. .bash_profile
 
 # Install gems
 gem install rails --version "$RAILSBRIDGE_RAILS_VERSION"
