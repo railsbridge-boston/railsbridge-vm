ec0f70890e261cf1a096d41e51297a6a97bf8767
Decklin Foster <decklin@red-bean.com>
2015-01-22 12:09:00 -0500

    Use a commit ref to download a newer ruby-install (so we get 2.0.0-p598)

--- versions.sh
+++ versions.sh
@@ -2,7 +2,12 @@
 RAILSBRIDGE_RUBY_VERSION='2.0'
 RAILSBRIDGE_RAILS_VERSION='4.0.13'
 
-# These are used for installing/selecting Ruby and may need to be bumped
-# for new patchlevels, but should not otherwise need to be changed.
+# We use ruby-install to build Ruby. Before building the VM image, check
+# https://github.com/postmodern/ruby-install/commits/master for the
+# latest update to the checksums for the Ruby version defined above, and
+# update the commit here. This is necessary because ruby-install is not
+# tagged for each new Ruby release (the latest tag is currently 0.5.0).
+RAILSBRIDGE_RUBY_INSTALL_REVISION='536dca4af4bb18f1f605356c2055a745b7bb3222'
+# After Ruby is installed we use chruby to enable it for the Vagrant user.
+# We can use a tag for this.
 RAILSBRIDGE_CHRUBY_VERSION='0.3.9'
-RAILSBRIDGE_RUBY_INSTALL_VERSION='0.5.0'
