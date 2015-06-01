a07b96e686eb6f4e3aa10ff03fdf430b10c4275a
Decklin Foster <decklin@red-bean.com>
2015-06-01 13:38:03 -0400

    Update Ruby version to 2.2.2

--- versions.sh
+++ versions.sh
@@ -1,5 +1,5 @@
 # Change these whenever the curriculum is updated.
-RAILSBRIDGE_RUBY_VERSION='2.2.1'
+RAILSBRIDGE_RUBY_VERSION='2.2.2'
 RAILSBRIDGE_RAILS_VERSION='4.0.13'
 
 # We use ruby-install to build Ruby. Before building the VM image, check
@@ -7,7 +7,7 @@ RAILSBRIDGE_RAILS_VERSION='4.0.13'
 # latest update to the checksums for the Ruby version defined above, and
 # update the commit here. This is necessary because ruby-install is not
 # tagged for each new Ruby release (the latest tag is currently 0.5.0).
-RAILSBRIDGE_RUBY_INSTALL_REVISION='146a92c50c12af7b610f4aee0520103b957629b4'
+RAILSBRIDGE_RUBY_INSTALL_REVISION='e2edd47e7e46b33592a0e990d25fe5046d6cb081'
 # After Ruby is installed we use chruby to enable it for the Vagrant user.
 # We can use a tag for this.
 RAILSBRIDGE_CHRUBY_VERSION='0.3.9'
