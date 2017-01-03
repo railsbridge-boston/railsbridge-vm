d1a39fc04b3d538ba3bc97e3c9298dfb07e23b14
Decklin Foster <decklin@red-bean.com>
2017-01-03 11:19:23 -0500

    Bump Ruby and ruby-install (no Rails update, 4.2.7.1 not compatible with 2.4.0)

--- provision-files/versions.sh
+++ provision-files/versions.sh
@@ -1,12 +1,12 @@
 # Change these whenever the curriculum is updated.
-RAILSBRIDGE_RUBY_VERSION='2.3.1'
+RAILSBRIDGE_RUBY_VERSION='2.3.3'
 RAILSBRIDGE_RAILS_VERSION='4.2.7.1'
 
 # We use ruby-install to build Ruby. Before building the VM image, check
 # https://github.com/postmodern/ruby-install/commits/master to see if
 # there is a tag with the Ruby version we want. If not, change this back
 # to a revision.
-RAILSBRIDGE_RUBY_INSTALL_VERSION='0.6.0'
+RAILSBRIDGE_RUBY_INSTALL_VERSION='0.6.1'
 # After Ruby is installed we use chruby to enable it for the Vagrant user.
 # We can use a tag for this.
 RAILSBRIDGE_CHRUBY_VERSION='0.3.9'
